2b5312ec582d887141dd264076b3d4c15d729f14
berke ersiz <berkrsz@gmail.com>
2021-03-29 17:23:09 +0300

    29 Mart Güncellemesi

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -10,5 +10,8 @@ namespace Business.Constants
         public static string ProductNameInvalid = "Ürün ismi geçersiz.";
         public static string MaintenanceTime = "Sistem bakım durumunda.";
         public static string ProductsListed = "Ürünler listelendi.";
+        public static string ProductCountCategoryError = "Kategori sayısı maksimim 10 olabilir.";
+        public static string ProductNameAlreadyExists = "Bu isimde bir ürün zaten var.";
+        public static string CategoryLimitExceded = "Kategori limiti aşıldığı için yeni ürün eklenemiyor.";
     }
 }
